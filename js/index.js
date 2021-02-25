8ce518c39d5f82ccb252ffbd25e085881d2d842d
Irene A <ihatepineapple@gmail.com>
2021-02-25 14:23:54 +0100

    adding items and checked functions ok

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 let todoItems = [];
 
+const renderTodo = (todo) => {
+    const list = document.querySelector('.todo-list');
+    const isChecked = todo.checked ? 'done': '';
+
+    const node = document.createElement("li");
+
+    node.setAttribute('class', `todo-item ${isChecked}`);
+    node.setAttribute('data-key', todo.id);
+
+    node.innerHTML = `
+    <input id="${todo.id}" type="radio"/>
+    <span>${todo.text}</span>
+
+    <i class="far fa-times-circle delete-todo js-delete-todo"></i>
+    `;
+
+    list.append(node);
+
+}
+
 const addTodo = (text) => {
     const todo = {
         text,
@@ -8,5 +28,21 @@ const addTodo = (text) => {
     };
 
     todoItems.push(todo);
-    console.log(todoItems)
-}
+    console.log(todoItems);
+    renderTodo(todo)
+    
+};
+
+const form = document.querySelector('.entry-form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const input = document.querySelector('.todo-input');
+   
+    const text = input.value.trim();
+    if (text !== '') {
+      addTodo(text);
+      input.value = '';
+      input.focus();
+    }
+  });
