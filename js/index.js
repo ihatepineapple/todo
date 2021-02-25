dcf3c96c2f6301dffc03af3e78c2952b6109b907
Irene A <ihatepineapple@gmail.com>
2021-02-25 13:31:29 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let todoItems = [];
+
+const addTodo = (text) => {
+    const todo = {
+        text,
+        checked: false,
+        id: Date.now()
+    };
+
+    todoItems.push(todo);
+    console.log(todoItems)
+}
